9c73ed4c7244ce1abbd092c6d980f67511dcae77
RIFAIGGZ <76506048+rifaiganz@users.noreply.github.com>
2021-04-18 14:41:17 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,208 @@
+const help = (prefix) => {
+	return `[  *Menu Bot Ryz*  ]
+	 *Hallow ${pushname}*
+	 *Battery Bot :  ${batterylevel}%*
+	 *Waktu :* *${moment.unix(int.timestamp).format('HH:mm:ss')}*
+	*Menu Teratas*
+⇗ *${prefix}sticker (reply gambar)*
+⇗ *${prefix}antidelete aktif/mati*
+⇗ *${prefix}ncheck (1 / 0)*
+⇗ *${prefix}autosticker (1 / 0)*
+⇗ *${prefix}fakedeface link | text | desk*
+⇗ *${prefix}kusonimesearch query*
+⇗ *${prefix}otakudesusearch query*
+⇗ *${prefix}spotify link*
+⇗ *${prefix}spotifysearch query*
+⇗ *${prefix}nekopoi link nekopoi*
+⇗ *${prefix}tiktokstalk name akun*
+⇗ *${prefix}tiktok link*
+⇗ *${prefix}xnxx query*
+⇗ *${prefix}nhentaipdf code*
+⇗ *${prefix}tebakgambar*
+⇗ *${prefix}tebakkata*
+⇗ *${prefix}caklontong*
+⇗ *${prefix}family100*
+⇗ *${prefix}img2url (reply image)*
+⇗ *${prefix}semoji emoji*
+⇗ *${prefix}tiktoknowm link*
+⇗ *${prefix}telesticker link*
+⇗ *${prefix}igdl link*
+⇗ *${prefix}fbdl link*
+⇗ *${prefix}drakorongoing*
+⇗ *${prefix}lk21 query*
+⇗ *${prefix}xnxxsearch query*
+⇗ *${prefix}togif (reply sticker bergerak)*
+⇗ *${prefix}meme (lelucon indo)*
+⇗ *${prefix}darkjokes (lelucon gelap :V)*
+⇗ *${prefix}character karakter anime*
+⇗ *Xaddtext text | text (reply gambar)*
+⇗ *Xnulis nama&kelas&kata*
+⇗ *Xytsearch query*
+⇗ *Xgetmusic id*
+⇗ *Xgiftlimit*
+⇗ *Xsuit*
+⇗ *Xmancing*
+⇗ *Xevent 1 / 0*
+⇗ *Xafk reason*
+⇗ *Xantivirtex 1 / 0*
+⇗ *antibadword on / off*
+⇗ */antilinkgroup 1 / 0*
+⇗ */leveling enable / disable*
+⇗ */level (liat level)*
+⇗ */mining*
+⇗ */jualikan (jumblah)*
+⇗ */transfer @|jumblah*
+⇗ */imagesearch query*
+⇗ */searchsticker query*
+
+
+         *Menu Sampingan*
+                */neko*
+                 */waifu*
+                 */loli*
+                 */chiisaihentai*
+                 */trap*
+                 */blowjob*
+                 */yaoi*
+                 */ecchi*
+                 */hentai*
+                 */ahegao*
+                 */hololewd*
+                 */sideoppai*
+                 */animefeets*
+                 */animebooty*
+                 */animethighss*
+                 */hentaiparadise*
+                 */animearmpits*
+                 */hentaifemdom*
+                 */lewdanimegirls*
+                 */biganimetiddies*
+                 */animebellybutton*
+                 */hentai4everyone*
+
+           *Menu Logo*
+  
+*[   Prefix = /   ]*
+*Gunakan /command *
+*Contoh /neon textnya , /advanceglow textnya*
+
+\`\`\` - Text Pro Me
+  ⤷ Blackpink
+  ⤷ Neon
+  ⤷ Green Neon
+  ⤷ Advance Glow
+  ⤷ Future Neon
+  ⤷ Sand Writing
+  ⤷ Sand Summer
+  ⤷ Sandengraved
+  ⤷ Metal Dark
+  ⤷ Neon Light
+  ⤷ Holographic
+  ⤷ Text 1917
+  ⤷ Minion
+  ⤷ Deluxe Silver
+  ⤷ New Year Card
+  ⤷ Blood Frosted
+  ⤷ Halloween
+  ⤷ Joker Logo
+  ⤷ Firework Sparkle
+  ⤷ Nature Leaves
+  ⤷ Bokeh
+  ⤷ Toxic
+  ⤷ Strawberry
+  ⤷ Box 3D
+  ⤷ Road Warning
+  ⤷ Break Wall
+  ⤷ Ice Cold
+  ⤷ Luxury
+  ⤷ Cloud
+  ⤷ Summersand
+  ⤷ Horror Blood
+  ⤷ Thunder
+  ⤷ Pornhub
+  ⤷ Glitch
+  ⤷ Avenger
+  ⤷ Space
+  ⤷ Ninja Logo
+  ⤷ Marvel Studio
+  ⤷ Lion Logo
+  ⤷ Wolf Logo
+  ⤷ Steel 3D
+  ⤷ Wall Gravity
+
+- Photo Oxy
+  ⤷ Shadow
+  ⤷ Cup
+  ⤷ Cup 2
+  ⤷ Romance
+  ⤷ Smoke
+  ⤷ Burn Paper
+  ⤷ Love Message
+  ⤷ Under Grass
+  ⤷ Love
+  ⤷ Coffe
+  ⤷ Wood Heart
+  ⤷ Wooden Board
+  ⤷ Summer 3D
+  ⤷ Wolf Metal
+  ⤷ Nature 3D
+  ⤷ Under Water
+  ⤷ Golden Rose
+  ⤷ Summer Nature
+  ⤷ Letter Leaves
+  ⤷ Glowing Neon
+  ⤷ Fall Leaves
+  ⤷ Flamming
+  ⤷ Harry Potter
+  ⤷ Carved Wood
+  ⤷ Tiktok
+  ⤷ Arcade 8bit
+  ⤷ Battlefield 4
+  ⤷ PUBG
+  ⤷ Banner LOL
+
+- Ephoto 360
+  ⤷ Wet Glass
+  ⤷ Multicolor 3D
+  ⤷ Water Color		
+  ⤷ Luxury Gold
+  ⤷ Galaxy Wallpaper
+  ⤷ Light Text
+  ⤷ Beautiful Flower
+  ⤷ Puppy Cute
+  ⤷ Royal Text
+  ⤷ Heart Shaped
+  ⤷ Birthday Cake
+  ⤷ Galaxy Style
+  ⤷ Hologram 3D
+  ⤷ Green Neon
+  ⤷ Glossy Chrome
+  ⤷ Green Bush
+  ⤷ Metal Logo
+  ⤷ Noel Text
+  ⤷ Glitter Gold
+  ⤷ Text Cake
+  ⤷ Stars Night
+  ⤷ Wooden 3D
+  ⤷ Text by Name
+  ⤷ Write Galaxy
+  ⤷ Galaxy Bat
+  ⤷ Snow 3D
+  ⤷ Birthday Day
+  ⤷ Gold Play Button
+  ⤷ Silver Play Button
+  ⤷ Free Fire \`\`\`
+
+MAKER MENU
+\`\`\`anakharam
+heker
+fftourserti
+fftourserti2
+fftourserti3
+fftourserti4
+fftourserti5
+\`\`\`
+`
+}
+
+exports.help = help
